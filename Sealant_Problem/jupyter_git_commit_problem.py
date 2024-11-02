db5e4227f442ff32ab54738ef799e9b89a777dc9
Jason Park <developerha0013@gmail.com>
2024-11-02 12:03:07 +0900

    update

---
+++ Sealant_Problem/jupyter_git_commit_problem.py
@@ -0,0 +1,15 @@
+import nbformat
+
+notebook_path = "C:/junha/Artificial_Intelligence/Sealant_Problem/Sealant_Problem_OverSampling_SwinTransformer.ipynb"
+
+with open(notebook_path, "r", encoding="utf-8") as f:
+    notebook = nbformat.read(f, as_version=4)
+
+for cell in notebook.cells:
+    if cell.cell_type == "code" and "execution_count" not in cell:
+        cell.execution_count = None
+
+with open(notebook_path, "w", encoding="utf-8") as f:
+    nbformat.write(notebook, f)
+
+print("Notebook Modified")
